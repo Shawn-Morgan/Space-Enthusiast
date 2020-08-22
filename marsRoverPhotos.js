8c527748697b5048bdad7d9d401ca2a42d0c57fc
shawnm7474 <shawnm74@gmail.com>
2020-08-22 12:50:49 -0500

    fixed search by rover camera, removed date picker for now

--- marsRoverPhotos.js
+++ marsRoverPhotos.js
@@ -1,17 +1,17 @@
-//get mars Rover photos...
+// //get mars Rover photos...
 
-var searchButton = $("#run-search");
-var rovers = ["Curiosity", "Opportunity", "Spirit"]
-//queryMarsRoverUrlByDate = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=" + nasaApiKey
+// var searchButton = $("#run-search");
+// var rovers = ["Curiosity", "Opportunity", "Spirit"]
+// //queryMarsRoverUrlByDate = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=" + nasaApiKey
 
-//function displayRoverInfo() {
+// //function displayRoverInfo() {
     
-    var nasaApiKey = "h2ElaeW9thH3j4eYaQySO8pbLQCzsK6aeiYYd3sU"
-    var rover = $(this).attr("data-name");
-    queryMarsRoverUrlByDate = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=" + nasaApiKey
-    //queryMarsRoverUrlByDate = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?earth_date=2015-6-3&api_key=" + nasaApiKey
+//     var nasaApiKey = "h2ElaeW9thH3j4eYaQySO8pbLQCzsK6aeiYYd3sU"
+//     var rover = $(this).attr("data-name");
+//     queryMarsRoverUrlByDate = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=" + nasaApiKey
+//     //queryMarsRoverUrlByDate = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?earth_date=2015-6-3&api_key=" + nasaApiKey
 
-
+//prevent future dates being selected
 // var $j=jQuery.noConflict()
 // $j("#year").datepicker({
 //     endDate: new Date()
@@ -28,17 +28,24 @@ $("#run-search").on("click", function(event) {
     //URL to search nasa
 
     var roverName = $("#rover").val()
-
     var roverCamera = $("#rovercamera").val()
-
     var earthDay = $("#year").val()
 
-    queryMarsRoverUrlByDate = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + roverName + "/photos?earth_date=" + earthDay + "&api_key=" + nasaApiKey
+    //query by date
+    queryMarsRoverUrlByDate = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + roverName + "/photos?earth_date=" + earthDay + "&camera=" + roverCamera + "&api_key=" + nasaApiKey
+    //+manifest 
+    queryMarsRoverManifest = "https://api.nasa.gov/mars-photos/api/v1/manifests/" + roverName + "?&api_key=" + nasaApiKey
+    //query sol=100
+    queryMarsRoverUrlBySol = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + roverName + "/photos?sol=100&camera=" + roverCamera + "&api_key=" + nasaApiKey
     console.log(queryMarsRoverUrlByDate);
+    console.log("------------------------------------");
+    console.log(queryMarsRoverManifest);
+    console.log("------------------------------------");
+    console.log(queryMarsRoverUrlBySol);
 
     //perform the ajax request
     $.ajax({
-        url: queryMarsRoverUrlByDate,
+        url: queryMarsRoverUrlBySol,
         method: "GET"
         })
             //data returned from api
@@ -49,7 +56,7 @@ $("#run-search").on("click", function(event) {
             function checkCamera(photo) {
                 return photo.camera.name == roverCamera;
             }
-            
+
             response.photos.filter(checkCamera);
 
             $("#photos").html("<img src=" + response.photos[0].img_src + ">");
@@ -68,3 +75,33 @@ $("#run-search").on("click", function(event) {
     });
 
 });
+
+
+
+
+//pulls back all photos available, but would need to limit photos displayed...
+// $("#run-search").on("click", function(event) {
+//     event.preventDefault();
+//     //refers to the button that was clicked
+//     var rover = $(this).attr("data-name");
+//     //APIkey
+//     var nasaApiKey = "h2ElaeW9thH3j4eYaQySO8pbLQCzsK6aeiYYd3sU";
+//     var date = document.querySelector('input[type="date"]').value;
+//     var rover = document.getElementById('rover').value;;
+//     queryMarsRoverUrlByDate = "https://api.nasa.gov/mars-photos/api/v1/rovers/"+rover+"/photos?earth_date="+date+"&api_key=" + nasaApiKey
+//     //perform the ajax request
+//     $.ajax({
+//         url: queryMarsRoverUrlByDate,
+//         method: "GET"
+//         })
+//             //data returned from api
+//             .then(function(response) {
+//             console.log(response);
+//             for (var i = 0; i < response.photos.length; i++) {
+//                 var mealDiv = $("#photos");
+//                 var imgURL = response.photos[i].img_src;
+//                 var image = $("<img>").attr("src", imgURL);
+//                 mealDiv.append(image);
+//             };
+//     });
+// });
