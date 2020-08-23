f2c49caaad1d0f5230668828536ebadd1e811f19
shawnm7474 <shawnm74@gmail.com>
2020-08-23 18:21:39 -0500

    small modifications and cleanup

--- marsRoverPhotos.js
+++ marsRoverPhotos.js
@@ -1,23 +1,3 @@
-// //get mars Rover photos...
-
-// var searchButton = $("#run-search");
-// var rovers = ["Curiosity", "Opportunity", "Spirit"]
-// //queryMarsRoverUrlByDate = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=" + nasaApiKey
-
-// //function displayRoverInfo() {
-    
-//     var nasaApiKey = "h2ElaeW9thH3j4eYaQySO8pbLQCzsK6aeiYYd3sU"
-//     var rover = $(this).attr("data-name");
-//     queryMarsRoverUrlByDate = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=" + nasaApiKey
-//     //queryMarsRoverUrlByDate = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?earth_date=2015-6-3&api_key=" + nasaApiKey
-
-//prevent future dates being selected
-// var $j=jQuery.noConflict()
-// $j("#year").datepicker({
-//     endDate: new Date()
-// });
-
-////////////////////////
 //event listner rover PHOTOS
 $("#run-search").on("click", function(event) {
     event.preventDefault();
@@ -108,33 +88,12 @@ $("#run-manifest").on("click", function(event) {
             .then(function(response) {
             console.log(response);
 
-            // Transfer content to HTML
+            //transfer content to HTML
             $(".name").text("Rover Name: " + response.photo_manifest.name);
             $(".status").text("Rover Status: " + response.photo_manifest.status);
             $(".launch_date").text("Rover Launch Date: " + response.photo_manifest.launch_date);
             $(".landing_date").text("Rover Landing Date: " + response.photo_manifest.landing_date);
             $(".max_date").text("Interesting Fact: the most recent Earth Date from which photos exist was: " + response.photo_manifest.max_date);
-
-            //e...
-            //filter function
-            // function checkCamera(photo) {
-            //     return photo.camera.name == roverCamera;
-            // }
-
-            // response.photos.filter(checkCamera);
-
-            // $("#photos").html("<img src=" + response.photos[0].img_src + ">");
-            // console.log(response.photos[0].img_src);
-            // var photosDiv = $("<div id='#photos'>");
-
-            // // Retrieving the URL for the image
-            // var imgURL = response.photos[0].img_src;
-
-            // // Creating an element to hold the image
-            // var image = $("<img>").attr("src", imgURL);
-        
-            // // Appending the image
-            // photosDiv.append(image);
 
     });
 
